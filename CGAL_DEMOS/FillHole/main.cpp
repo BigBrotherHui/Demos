12e746886c66dfd86334ae99af23324fbcff9b88
zhoushenghui <1727711853@qq.com>
2023-12-05 17:36:22 +0800

    更新代码

--- CGAL_DEMOS/FillHole/main.cpp
+++ CGAL_DEMOS/FillHole/main.cpp
@@ -13,10 +13,10 @@ typedef CGAL::Polyhedron_3<Kernel> Polyhedron_3;
 namespace PMP = CGAL::Polygon_mesh_processing;
 namespace params = PMP::parameters;
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-typedef CGAL::Surface_mesh<K::Point_3> Surface_mesh;
+typedef CGAL::Surface_mesh<K::Point_3> SurfaceMesh;
 typedef CGAL::Polyhedron_3<K> Polyhedron;
-typedef Surface_mesh::Vertex_index vertex_descriptor;
-typedef Surface_mesh::Face_index face_descriptor;
+typedef SurfaceMesh::Vertex_index vertex_descriptor;
+typedef SurfaceMesh::Face_index face_descriptor;
 typedef CGAL::Surface_mesh<Kernel::Point_3> Mesh;
 typedef Mesh::Property_map<face_descriptor, double> Face_area_map;
 #define dot(u,v)   ((u).x() * (v).x() + (u).y() * (v).y() + (u).z() * (v).z())
@@ -44,20 +44,23 @@ struct Compute_area
 };
 int main()
 {
-    //构造平面
-    //CGAL::Plane_3 cut_plane(0, 0, 1, 0);
-
     //读取模型
 	std::string stl_file_name = "D:/Demos_BUILD/lib/x64/Release/stl/Femur_left.stl";
-	Polyhedron_3 poly_Partition;
-	CGAL::Polygon_mesh_processing::IO::read_polygon_mesh(stl_file_name, poly_Partition);
-    Mesh mesh;
-    CGAL::copy_face_graph(poly_Partition, mesh);
-    for (const auto& face : mesh.faces()) {
-        auto ar = CGAL::Polygon_mesh_processing::area(face, mesh);
-        // std::cout << "Face area: " << ar.exact() << std::endl;
+    
+    Polyhedron_3 poly_Partition;
+    CGAL::Polygon_mesh_processing::IO::read_polygon_mesh(stl_file_name, poly_Partition);
+    poly_Partition.normalize_border();
+    SurfaceMesh surface_mesh;
+    CGAL::copy_face_graph(poly_Partition,surface_mesh);
+    auto visited = surface_mesh.halfedges();
+    std::map<SurfaceMesh::halfedge_index, bool> mp;
+    for(auto h: visited)
+    {
+	    if(surface_mesh.is_border(h)&&!mp[h])
+	    {
+		    
+	    }
     }
-    //PMP::stitch_borders(poly_Partition, CGAL::parameters::vertex_point_map(get(CGAL::vertex_point, poly_Partition)));
 #if defined(CGAL_TEST_SUITE)
     bool cgal_test_suite = true;
 #else
