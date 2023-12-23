496bebadec2b75113b244d01846d8999abedee6f
unknown <1727711853@qq.com>
2023-12-23 18:07:18 +0800

    提交DrawApp

---
+++ Modules/PropertyBrowser/src/PropertyBrowser_global.h
@@ -0,0 +1,10 @@
+﻿#ifndef PROPERTYBROWSER_GLOBAL_H
+#define PROPERTYBROWSER_GLOBAL_H
+
+#ifdef PROPERTYBROWSER_DLL
+#define PROPERTYBROWSER_EXPORT __declspec(dllexport)
+#else
+#define PROPERTYBROWSER_EXPORT __declspec(dllimport)
+#endif
+
+#endif
